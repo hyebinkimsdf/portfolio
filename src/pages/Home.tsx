b78508c3d268f83ffc603ef803ac96329410a58e
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-11 19:14:02 +0900

    design: Add video BG white filter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen relative overflow-hidden">
       {/* 배경 비디오 */}
+
       <video
         className="absolute top-0 left-0 w-full h-full object-cover "
         src="/videos/main-bg-simple.mp4"
@@ -12,9 +13,9 @@ export default function Home() {
         loop
         muted
       />
-
+      <div className="bg-white opacity-70 absolute w-screen h-screen"></div>
       {/* 카드 컴포넌트 (비디오 위에 배치) */}
-      <div className="absolute z-10 flex justify-center items-center w-full h-full gap-4">
+      <div className="absolute flex justify-center items-center w-full h-full gap-4">
         <ProjectCard name="eyecane" link="/Eyecane" />
       </div>
     </div>
