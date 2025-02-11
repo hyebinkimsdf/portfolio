1ea1c7ec5041ddff784c0d0f62c8e48bc3a95b65
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-11 18:54:20 +0900

    design: Header 반응형 수정

--- src/compoments/home/Header.tsx
+++ src/compoments/home/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <div className="bg-black w-full h-10  text-white px-4 felx items-center justify-center">
+    <div className="bg-black w-full h-[10vw] max-h-[64px]  text-white px-4 flex items-center">
       <Link to="/">main</Link>
     </div>
   );
