db6caf241a6da2aa1c156de9ae0da37af1003042
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-11 18:33:51 +0900

    design: App.tsx Headercompoment fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
 import Eyecane from "./pages/Eyecane";
+import { Header } from "./compoments/home/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Eyecane" element={<Eyecane />} />
-    </Routes>
+    <div className="app">
+      <Header />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Eyecane" element={<Eyecane />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ src/compoments/home/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="bg-yellow-200 w-full h-10 ">
+      <p>main</p>
+      <p>about</p>
+    </div>
+  );
+}
