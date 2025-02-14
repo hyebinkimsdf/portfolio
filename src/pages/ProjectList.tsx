89733ad7401e27cf0d5150b5623f326910c3a0be
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-14 19:03:27 +0900

    fix: projectCard name, 경로 변경

--- src/pages/ProjectList.tsx
+++ src/pages/ProjectList.tsx
@@ -7,11 +7,15 @@ export function ProjectList() {
 
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-center gap-8">
+          <ProjectCard name={"Naver고객센터"} link={"/naver"} image={""} />
           <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
-          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
-          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
-          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
-          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
+          <ProjectCard
+            name={"드론 데이터 시각화 웹사이트"}
+            link={"/dronGo"}
+            image={""}
+          />
+          <ProjectCard name={"수영복 아커머스"} link={"/swim360"} image={""} />
+          <ProjectCard name={"아이돌 팬사이트"} link={"/idolfan"} image={""} />
         </div>
       </div>
     </div>
