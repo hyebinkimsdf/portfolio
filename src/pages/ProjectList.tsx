125905b81b8292985511d18783675fff7aea502c
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-14 18:52:31 +0900

    design: ProjectList 컨테이너 범위 추가

--- src/pages/ProjectList.tsx
+++ src/pages/ProjectList.tsx
@@ -2,12 +2,18 @@ import { ProjectCard } from "../compoments/home/ProjectCard";
 
 export function ProjectList() {
   return (
-    <div className="w-full min-h-screen items-center flex justify-center mx-auto flex-wrap bg-white">
-      <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
-      <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
-      <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
-      <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
-      <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
+    <div className="w-full min-h-screen bg-white py-10">
+      <p className="text-center font-bold text-2xl mb-12">Projects</p>
+
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-center gap-8">
+          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
+          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
+          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
+          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
+          <ProjectCard name={"Eyecane"} link={"/eyecane"} image={""} />
+        </div>
+      </div>
     </div>
   );
 }
