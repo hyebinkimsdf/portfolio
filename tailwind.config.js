301a1aee84143fbc796ff8b4243c05890e44fb8d
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-25 01:08:36 +0900

    design: 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard", "Apple SD Gothic Neo", "sans-serif"],
+        // 원하는 다른 폰트 패밀리도 추가 가능
+      },
+    },
   },
   plugins: [],
 };
